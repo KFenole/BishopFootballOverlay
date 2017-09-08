edd22ab906bc60a97967d5bb991471cf8b70cf78
Kyle Fenole <kyle@kfenole.com>
2017-09-08 16:09:16 -0700

    Initial Commit

---
+++ ScoreOverlayFootball/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScoreOverlayFootball
+//
+//  Created by Kyle Fenole on 9/8/17.
+//  Copyright © 2017 Kyle Fenole. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
